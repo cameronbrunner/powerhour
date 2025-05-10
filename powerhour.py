0fbaf3cbc1419d050a42d02954bf68523a2bda95
Cameron Brunner <cameron@hbrunner.com>
2025-05-09 23:37:26 -0500

    Fixed some basic bugs

--- powerhour.py
+++ powerhour.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 
 from synochat.webhooks import SlashCommand
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ if [ ! -f env.sh ]; then
   touch env.sh
 fi
 
-sudo docker run -p 5001:5001 -d --restart=on-failure  -env-file=env.sh --device=/dev/i2c-1 --device=/dev/i2c-2 --name powerhour  powerhour
+sudo docker run -p 5001:5001 -d --restart=on-failure --env-file=env.sh --device=/dev/i2c-1 --device=/dev/i2c-2 --name powerhour  powerhour
